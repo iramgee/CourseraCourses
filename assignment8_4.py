d3ca212c6eaef7d8899cde69e7be4abcd05f432d
iramgee <garciairam@gmail.com>
2016-02-25 14:50:03 -0800

    Update assignment8_4.py
    This is the working file for Coursera chapter 8.4

--- assignment8_4.py
+++ assignment8_4.py
@@ -1,19 +1,18 @@
-# fh = open('romeo.txt')
-# my_list = ['Arise', 'But', 'It', 'Juliet', 'Who', 'already', 'and', 'breaks', 'east', 'envious', 'fair', 'grief', 'is', 'kill', 'light', 'moon', 'pale', 'sick', 'soft', 'sun', 'the', 'through', 'what', 'window', 'with', 'yonder']
-# lst = list()
+fname = raw_input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    line = line.strip()
+    line = line.split()
+    
+    for words in line:
+        if words not in lst: #comparing words
+            lst.append(words)
+        else: continue
+lst.sort()
+print lst
 
-# for line in fh:
-#     lst = line.split() # finding words in this file
-#     lst.sort()
-# print lst
-
-alist = ['Arise But It Juliet breaks east envious fair kill light moon soft sun the through what window yonder']
-# fname = raw_input("Enter file name: ")
-# fh = open(fname)
-fh = open('romeo.txt')
-count = 0 # ease of use
-lst = list(fh)
-for line in lst:
-    words  = line.split() # finding all the words in this file
-
-    print words
+''' As it is going through it's loop and finds the words,
+it goes through a new loop and checks if any of the words are
+in my lst file. If they are not in the lst file, then it adds
+them. After that simply sort and print the words! '''
