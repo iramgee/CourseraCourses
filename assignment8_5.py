88e9c406553a99cd67ada483e3a8beddec171406
iramgee <garciairam@gmail.com>
2016-02-25 16:31:42 -0800

    Added files via upload

---
+++ assignment8_5.py
@@ -0,0 +1,14 @@
+fname = raw_input("Enter file name: ")
+if len(fname) < 1 : fname = "mbox-short.txt"
+fh = open(fname)
+count = 0
+for line in fh:
+    line = line.rstrip()
+    words = line.split()
+    if words == [] : continue
+    if words[0] != 'From'  : continue
+    count = count + 1
+    words = words[1]
+    print words
+
+print "There were", count, "lines in the file with From as the first word"
