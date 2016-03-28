9accfa98af4a7d6cc82641cd9e8b1785d8259839
Iram Garcia <garciairam@gmail.com>
2016-03-28 14:31:39 -0700

    made the loop a function

--- urllinks2.py
+++ urllinks2.py
@@ -1,15 +1,26 @@
-# Note - this code must run in Python 2.x and you must download
-# http://www.pythonlearn.com/code/BeautifulSoup.py
-# Into the same folder as this program
-
 import urllib
 from BeautifulSoup import *
 
-url = raw_input('Enter - ')
-html = urllib.urlopen(url).read()
-soup = BeautifulSoup(html)
 
-# Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-    print tag.get('href', None)
+def get_url_string():
+    count = 0
+    url = raw_input('Enter - ')
+    while count < 7:
+        print 'Successfully retrieved: ',url, '\n\n'
+        html = urllib.urlopen(url).read()
+        soup = BeautifulSoup(html)
+
+        # Retrieve all of the anchor tags
+        tags = soup('a')
+
+        lst = list()
+        count = count + 1
+        for tag in tags:
+            name = tag.get('href', None)
+            lst.append(name)
+    #    print count
+        url = lst[17] # grabbing url in 18th position
+        print 'Retrieving: ',url
+    print 'Successfully retrieved ',url 
+
+get_url_string()
